97a255d99a7bdfd0d5c31890ec151e4576ce69f2
Ryan <ryanjenkins1299@gmail.com>
2025-04-10 13:01:10 +0100

    initial commit

---
+++ api/v1alpha1/link_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type LinkSpec struct {
+	SourceCluster   string `json:"sourceCluster"`
+	TargetCluster   string `json:"targetCluster"`
+	SourceNamespace string `json:"sourceNamespace"`
+	TargetNamespace string `json:"targetNamespace"`
+	Applications    string `json:"applications"`
+	SecretNamespace string `json:"secretNamespace"`
+	SecretName      string `json:"secretName"`
+	SecretName2     string `json:"secretName2"`
+	Port            int    `json:"port"`
+}
+
+type LinkStatus struct {
+	State   string `json:"state,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+type Link struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LinkSpec   `json:"spec,omitempty"`
+	Status LinkStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+type LinkList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Link `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Link{}, &LinkList{})
+}
---
+++ internal/controller/ac3network_controller_test.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	ac3v1alpha1 "github.com/raycarroll/ac3no/api/v1alpha1"
+)
+
+var _ = Describe("AC3Network Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		ac3network := &ac3v1alpha1.AC3Network{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind AC3Network")
+			err := k8sClient.Get(ctx, typeNamespacedName, ac3network)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &ac3v1alpha1.AC3Network{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &ac3v1alpha1.AC3Network{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance AC3Network")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &AC3NetworkReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
